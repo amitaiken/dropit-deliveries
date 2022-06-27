4f621fdb477bfd0850716194883bbd7af3f1db31
amitai.ke <amitai.ke@voicenter.com>
2022-06-27 15:18:44 +0300

    Add function getFormattedAddress to users controller + fixes

--- routes/users/users.controler.js
+++ routes/users/users.controler.js
@@ -1,9 +1,24 @@
 let mysql = require('mysql');
-let config = require('/helper/config');
+let config = require('config');
 let connection = mysql.createConnection(config);
+const axios = require('axios').default;
 
-async function getUserFormattedAddress(addressData) {
-    
+
+async function getFormattedAddress(addressData, singleLine) {
+    const baseURL = 'https://maps.googleapis.com/maps/api/geocode/json?address='
+    const key = 'AIzaSyABp5ntY2-vgaLhBKyT4HWGiPXls1HAt8o';
+    let locationRequest;
+    if(!singleLine) {
+        const city = encodeURIComponent((addressData.City).trim());
+        const state = encodeURIComponent((addressData.State).trim());
+        const address = encodeURIComponent((addressData.Address).trim());
+        locationRequest = baseURL + address + '+' + city + '+' + state + '&key=' + key;
+    } else {
+        const address = encodeURIComponent((addressData).trim());
+        locationRequest = baseURL + address + '&key=' + key;
+    }
+    const formattedAddress = await axios(locationRequest);
+    return formattedAddress;
 }
 
 module.exports = class {
@@ -17,8 +32,8 @@ module.exports = class {
             const userID = parseInt(req.params['UserID']);
             let results = await connection.query("FN_UsersResolveAddress(?)",[userID]);
             const addressData = JSON.parse(results.Data);
-            const formattedAddress = await getUserFormattedAddress(addressData);
-            results = await connection.query("SP_UsersResolveAddress(?, ?)",[userID, addressData]);
+            const formattedAddress = await getFormattedAddress(addressData, 0);
+            results = await connection.query("SP_UsersResolveAddress(?, ?)",[userID, formattedAddress]);
             res.code(results.Status).send(results);
         } catch (error) {
             console.error(`Error in UsersResolveAddress: ${error.message}`);
@@ -29,12 +44,14 @@ module.exports = class {
             });
         }
     }
-    async FormattedAddressUpdate(req, res) {
+    async UsersFormattedAddressUpdate(req, res) {
         try {
-            const formattedAddress = JSON.parse(req.params['FormattedAddress']);
-
-            results = await connection.query("SP_UsersResolveAddress(?, ?)",[userID, formattedAddress]);
-            res.code(results.Status).send(addressData);
+            const singleLineAddress = req.params['Address'];
+            const userID = req.params['UserID'];
+            const formattedAddress = await getFormattedAddress(singleLineAddress, 1);
+            let results = await connection.query("SP_UsersResolveAddress(?, ?)",[userID, formattedAddress]);
+            results = JSON.parse(results);
+            res.code(results.Status).send(results);
         } catch (error) {
             console.error(`Error in FormattedAddressUpdate: ${error.message}`);
             res.code(500).send({
