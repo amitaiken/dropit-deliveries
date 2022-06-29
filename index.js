dc721bd5fa3ad6ebb22fa336d206eeacdb488a19
amitai.ke <amitai.ke@voicenter.com>
2022-06-29 17:39:32 +0300

    reorganize server files due to swagger fastify-openapi-glue model

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
-const fastify = require('fastify')({ logger: true })
 const fs = require('fs');
-const config = require('./config');
-const galeraCluster = require('@voicenter-team/mysql-dynamic-cluster');
-const cluster = galeraCluster.createPoolCluster(config.mysql);
+const config = require('config');
+const fastify = require('fastify')();
+const openapiGlue = require('fastify-openapi-glue');
+const log = require('./helpers/log');
 
 global['classes'] = {};
-
-const generatedControllersPath = `${__dirname}/routes_generated`;
-const generatedDefinitionsPath = `${__dirname}/definitions_generated`;
+const generatedControllersPath = `${__dirname}/generated/routes`;
+const generatedDefinitionsPath = `${__dirname}/generated/definitions`;
 const staticControllersPath = `${__dirname}/routes`;
 const staticDefinitionsPath = `${__dirname}/definitions`;
+
 const instances = [];
-
 // sync read of the generated definitions
-let gD = [];
+let generatedDefinitions = [];
 if (fs.existsSync(generatedDefinitionsPath)) {
-    gD = fs.readdirSync(generatedDefinitionsPath);
+    generatedDefinitions = fs.readdirSync(generatedDefinitionsPath);
 }
-
 // sync read of the static definitions
-let sD = [];
+let staticDefinitions = [];
 if (fs.existsSync(staticDefinitionsPath)) {
-    sD = fs.readdirSync(staticDefinitionsPath);
+    staticDefinitions = fs.readdirSync(staticDefinitionsPath);
 }
-
-// load static and generated definitions into the global scope TODO: improve
-gD.forEach((definition) => {
-    const source = sD.includes(definition) ? staticDefinitionsPath : generatedDefinitionsPath;
+// load static and generated definitions into the global scope
+// static definitions have preference over generator ones
+// TODO: improve
+generatedDefinitions.forEach((definition) => {
+    const source = staticDefinitions.includes(definition) ? staticDefinitionsPath : generatedDefinitionsPath;
+    // eslint-disable-next-line global-require,import/no-dynamic-require
     global.classes[definition] = require(`${source}/${definition}/${definition}.js`);
 });
-
-// sync read generated controllers
-let gC = [];
+let generatedControllers = [];
 if (fs.existsSync(generatedControllersPath)) {
-    gC = fs.readdirSync(generatedControllersPath);
+    generatedControllers = fs.readdirSync(generatedControllersPath);
 }
-
 // sync read stationary controllers
-let sC = [];
+let stationaryControllers = [];
 if (fs.existsSync(staticControllersPath)) {
-    sC = fs.readdirSync(staticControllersPath);
+    stationaryControllers = fs.readdirSync(staticControllersPath);
 }
-// load static and generated controllers TODO: improve
-gC.forEach((item) => {
+// load static and generated controllers
+// static definitions have preference over generator ones
+// TODO: improve
+generatedControllers.forEach((item) => {
     // load source file and get all of the available methods from the class
-    const source = sC.includes(item) ? staticControllersPath : generatedControllersPath;
+    const source = stationaryControllers.includes(item) ? staticControllersPath : generatedControllersPath;
+    // eslint-disable-next-line global-require,import/no-dynamic-require
     const Instance = require(`${source}/${item}/${item}.controller.js`);
     instances.push(Object.getOwnPropertyDescriptors(Instance.prototype));
 });
@@ -52,32 +52,59 @@ gC.forEach((item) => {
 // pass all of the methods from controllers to openapi-glue
 function Service() {
 }
-
 Service.prototype = {};
 instances.forEach((item) => {
+    // eslint-disable-next-line no-param-reassign
     delete item.constructor;
     Object.defineProperties(Service.prototype, item);
 });
-const service = new Service;
+const service = new Service();
 const swaggerOptions = {
     routePrefix: '/documentation',
     exposeRoute: true,
-    swagger: {...require(__dirname + '/docs/Swagger.v2.json'), ...config['swagger']}
+    // eslint-disable-next-line global-require,import/no-dynamic-require
+    swagger: { ...require(`${__dirname}/generated/swagger/Swagger.json`), ...config.swagger },
+};
+
+const openAPIGlueOptions = {
+    service,
+    specification: './generated/swagger/Swagger.json',
 };
 
 fastify
     .register(require('fastify-swagger'), swaggerOptions)
-// run the server
+    .register(openapiGlue, openAPIGlueOptions);
 
-const startWebService = async () => {
-    try {
-        await fastify.listen(3000);
-        fastify.log.info(`Server listening on ${fastify.server.address().port}, env: ${process.env.NODE_ENV}`)
-    } catch (err) {
-        fastify.log.error(err)
-        process.exit(1)
+fastify.addHook('onRequest', async (request) => {
+    log.info(`Request: ${request.raw.url}`);
+});
+
+fastify.addHook('onSend', async (request, reply, payload) => {
+    log.info(`Send: ${request.controllerName}`);
+    if (request.entityPermissionDenied) {
+        const response = JSON.parse(payload);
+        return JSON.stringify(response);
     }
-}
-startWebService();
+    return payload;
+});
+
+fastify.addHook('onClose', async (instance, done) => {
+    done();
+});
+
+fastify.get('/', (request, reply) => {
+    reply.send({ hello: 'world' });
+});
+
+fastify.listen(config.app.port, '0.0.0.0', (error) => {
+    if (error) {
+        console.error('server can\'t start listening', error);
+
+        process.exit(1);
+    }
+
+    log.info(`Server listening on ${config.app.port} env: ${process.env.NODE_ENV}`);
+});
 
 module.exports = fastify;
+
